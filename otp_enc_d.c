7248be8be6f3c39f20e72b50b3fcc97a15bd62f7
Andrius Kelly <kellandr@os1.engr.oregonstate.edu>
2018-03-13 17:02:10 -0700

    impliments encoding for single thread

--- otp_enc_d.c
+++ otp_enc_d.c
@@ -17,15 +17,19 @@
 
 #define BUF_LEN 256
 
+// PROTOTYPES
+char* encryptMessage(char* messageIn) ;
 
 void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
 
 int main(int argc, char* argv[]) {
 
 
-	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
+	int listenSocketFD, establishedConnectionFD, portNumber, charsRead, packetLength, messageSize, charsSent;
 	socklen_t sizeOfClientInfo;
 	char buffer[BUF_LEN];
+	char* messageIn;
+	char* messageOut;
 	struct sockaddr_in serverAddress, clientAddress;
 
 	// Check usage & arg
@@ -54,27 +58,108 @@ int main(int argc, char* argv[]) {
 
 	sizeOfClientInfo = sizeof(clientAddress);
 
+	//initialize incomming message to empty string
+	messageSize = 2*sizeof(buffer) + 1;
+	messageIn = malloc( messageSize);
+	messageIn[0] = '\0';
+
 	while(1) {
+
+		memset(messageIn, '\0', messageSize);
 
 		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr*)&clientAddress, &sizeOfClientInfo);
 		if (establishedConnectionFD < 0 ) { error("ERROR on accept");}	
 
 		charsRead = -1; 
-		//set charsRead on loop until client closes connection (charsRead == 0 )
-		while( charsRead != 0 ) {
+		//check incoming packet size	
+		//do {
 			//receive message
 			memset(buffer, '\0', BUF_LEN);
 			charsRead = recv( establishedConnectionFD, buffer, 255, 0);
 			if (charsRead < 0) { error("ERROR reading from socket");}
 
-			printf(buffer);
-			fflush(stdout);
-		}
+			//make sure input is read
+			do {
+				ioctl( establishedConnectionFD, FIONREAD, &packetLength );
+			} while (packetLength > 0);
+
+			//cat buffer to messageIn
+			if( messageSize <= strlen(buffer) + strlen(messageIn) ) {
+				messageSize *= 2;
+				messageIn = realloc(messageIn, messageSize);
+			}
+			strcat(messageIn, buffer);
+
+			messageOut = encryptMessage(messageIn);
+
+			//send encrypted message
+			charsSent = send(establishedConnectionFD, messageOut, strlen(messageOut), 0);
+			if (charsSent < 0 ) { error("ERROR writing to socket"); }
+
+			//make sure input is read
+			packetLength = -5;
+			do {
+				ioctl( establishedConnectionFD, TIOCOUTQ, &packetLength );
+			} while (packetLength > 0);
+
+
+		//} while( charsRead > 0 ); //read until client closes connection
 
 		close(establishedConnectionFD);
 		
 	}
 
+	free(messageOut);
+	free(messageIn);
 
 	return 0;
 }
+
+
+
+/*-------------------      FUNCTIONS     --------------------*/
+
+
+int cipher_ctoi( char c) {
+	if( c == ' ') 
+		return 26;
+	else 
+		return c - 'A';
+}
+
+char cipher(char msg_c, char key_c) {
+	int m, k, cipherInt;
+
+	//get int values for msg_c and key_c
+	m = cipher_ctoi(msg_c);
+	k = cipher_ctoi(key_c);
+
+	cipherInt = (m + k) % 27;
+
+	if( cipherInt == 26 )
+		return ' ';
+	else
+		return cipherInt + 'A';
+}
+
+char* encryptMessage(char* messageIn) {
+
+	int i;
+	char* messageOut;
+printf("%s\n", messageIn);
+	//set keyToken to point to start of key
+	char* keyToken = strtok(messageIn, "@");
+	keyToken = strtok(NULL, "@");
+	messageOut = (char*)malloc( strlen(messageIn + 1));
+
+	for( i=0; i<strlen(messageIn); i++ ) {
+		messageOut[i] = cipher(messageIn[i], keyToken[i]); 
+	}
+
+	messageOut[strlen(messageIn)] = '\0';
+
+printf("%s\n%s\n%s\n", messageIn, keyToken, messageOut);
+
+	return messageOut;
+}	
+
