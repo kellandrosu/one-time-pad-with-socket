8d33e4a25fb141f03c15ff579ddb8be46c499272
Andrius Kelly <kellandr@os1.engr.oregonstate.edu>
2018-03-14 12:15:07 -0700

    add child forking

--- otp_enc_d.c
+++ otp_enc_d.c
@@ -13,34 +13,59 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <sys/ioctl.h>
+#include <signal.h>
+#include <errno.h>
 
 
+#define true 1
+#define false 0
+#define CHILD_TOTAL 5
 #define BUF_LEN 256
+
+
+//GLOBALS
+pid_t child_pids[CHILD_TOTAL];
+
 
 // PROTOTYPES
 char* encryptMessage(char* messageIn) ;
+void communicateWithClient(int establishedConnectionFD) ;
+void catchSIGCHLD(int signo, siginfo_t* info, void* vp) ;
+void addChildProcess( pid_t pid);
+int removeChildProcess( pid_t pid);
+int isChildProcess( pid_t pid);
 
-void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+// Error function used for reporting issues
+void error(const char *msg) { fprintf(stderr,msg); exit(1); } 
+
+
+/*-------------    MAIN    --------------*/
+
 
 int main(int argc, char* argv[]) {
 
+	//process variables
+	pid_t spawnId;
 
-	int listenSocketFD, establishedConnectionFD, portNumber, charsRead, packetLength, messageSize, charsSent;
+	//SIGCHLD signal catching to kill zombie child
+	struct sigaction SIGCHLD_action = {0};
+
+	sigfillset(&SIGCHLD_action.sa_mask);
+	SIGCHLD_action.sa_flags = SA_SIGINFO | SA_RESTART;
+	SIGCHLD_action.sa_sigaction = catchSIGCHLD;
+	sigaction(SIGCHLD, &SIGCHLD_action, NULL);
+
+	//network variables
+	int listenSocketFD, establishedConnectionFD, portNumber;
 	socklen_t sizeOfClientInfo;
-	char buffer[BUF_LEN];
-	char* messageIn;
-	char* messageOut;
 	struct sockaddr_in serverAddress, clientAddress;
 
 	// Check usage & arg
 	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } 
-
 	portNumber = atoi(argv[1]); 
 	
-	//zero out serv addr struct
-	memset((char *)&serverAddress, '\0', sizeof(serverAddress));
-
 	// Set up the address struct for this process (the server)
+	memset((char *)&serverAddress, '\0', sizeof(serverAddress));
 	serverAddress.sin_family = AF_INET;
 	serverAddress.sin_port = htons(portNumber);
 	serverAddress.sin_addr.s_addr = INADDR_ANY;
@@ -53,65 +78,29 @@ int main(int argc, char* argv[]) {
 	if ( bind (listenSocketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0 ) {
 		error("ERROR on binding");
 	}
-
 	listen( listenSocketFD, 5); //open socket to listen with queue length of 5
 
 	sizeOfClientInfo = sizeof(clientAddress);
 
-	//initialize incomming message to empty string
-	messageSize = 2*sizeof(buffer) + 1;
-	messageIn = malloc( messageSize);
-	messageIn[0] = '\0';
-
 	while(1) {
-
-		memset(messageIn, '\0', messageSize);
 
 		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr*)&clientAddress, &sizeOfClientInfo);
 		if (establishedConnectionFD < 0 ) { error("ERROR on accept");}	
 
-		charsRead = -1; 
-		//check incoming packet size	
-		//do {
-			//receive message
-			memset(buffer, '\0', BUF_LEN);
-			charsRead = recv( establishedConnectionFD, buffer, 255, 0);
-			if (charsRead < 0) { error("ERROR reading from socket");}
-
-			//make sure input is read
-			do {
-				ioctl( establishedConnectionFD, FIONREAD, &packetLength );
-			} while (packetLength > 0);
-
-			//cat buffer to messageIn
-			if( messageSize <= strlen(buffer) + strlen(messageIn) ) {
-				messageSize *= 2;
-				messageIn = realloc(messageIn, messageSize);
-			}
-			strcat(messageIn, buffer);
-
-			messageOut = encryptMessage(messageIn);
-
-			//send encrypted message
-			charsSent = send(establishedConnectionFD, messageOut, strlen(messageOut), 0);
-			if (charsSent < 0 ) { error("ERROR writing to socket"); }
-
-			//make sure input is read
-			packetLength = -5;
-			do {
-				ioctl( establishedConnectionFD, TIOCOUTQ, &packetLength );
-			} while (packetLength > 0);
-
-
-		//} while( charsRead > 0 ); //read until client closes connection
-
-		close(establishedConnectionFD);
-		
+		//fork child processes
+		spawnId = fork();
+		switch(spawnId) {
+			case -1:
+				error("ERROR: process fork error");
+				break;
+			case 0:
+				communicateWithClient(establishedConnectionFD) ;
+				return 0;
+			default:
+				addChildProcess(spawnId);
+				break;
+		}
 	}
-
-	free(messageOut);
-	free(messageIn);
-
 	return 0;
 }
 
@@ -119,7 +108,142 @@ int main(int argc, char* argv[]) {
 
 /*-------------------      FUNCTIONS     --------------------*/
 
+/*
+	kills dead child processes
+*/
+void catchSIGCHLD(int signo, siginfo_t* info, void* vp) {
+	pid_t child_pid;
+	int childExitMethod = -5;
+	int result;
 
+	//check if calling pid is a child process
+	if ( isChildProcess(info->si_pid) ) {
+		//get status of calling process
+		child_pid = info->si_pid;
+		do { 
+			errno = 0;
+			result = waitpid(child_pid, &childExitMethod, WNOHANG);
+		} while (errno == EINTR && result == -1);
+
+		if (result == child_pid) {
+			//if child exited then need call sigterm to kill zombie
+			if( WIFEXITED(childExitMethod) != 0 ) {
+				removeChildProcess(child_pid);
+				kill(child_pid, SIGTERM);
+			}
+		}
+	}
+}
+
+//BG process array functions
+void addChildProcess( pid_t pid) {
+    int i;
+    for (i=0; i < CHILD_TOTAL; i++) {
+        if ( child_pids[i] == 0) {
+            child_pids[i] = pid;
+            break;
+}   }   }
+int removeChildProcess( pid_t pid) {
+    int i;
+    for (i=0; i < CHILD_TOTAL; i++) {
+        if ( child_pids[i] == pid) {
+            child_pids[i] = 0;
+            return true;
+    }   }
+    return false;
+}
+int isChildProcess( pid_t pid) {
+    int i;
+    for (i=0; i < CHILD_TOTAL; i++) {
+        if ( child_pids[i] == pid) {
+            return true;
+    }   }
+    return false;
+}
+
+
+/*
+	receives message with key separated by @, 
+	sends back encrypted message and closes connection
+*/
+void communicateWithClient(int establishedConnectionFD) {
+	
+	int charsRead, packetLength, messageSize, charsSent;
+	char buffer[BUF_LEN];
+	char* messageIn;
+	char* messageOut;
+	
+	//initialize incomming message to empty string
+	messageSize = 2*sizeof(buffer) + 1;
+	messageIn = malloc( messageSize);
+	memset(messageIn, '\0', messageSize);
+	
+	charsRead = -1; 
+			
+	//receive message
+	memset(buffer, '\0', BUF_LEN);
+	charsRead = recv( establishedConnectionFD, buffer, BUF_LEN - 1, 0);
+	if (charsRead < 0) { error("ERROR reading from socket");}
+
+	//make sure input is read
+	do {
+		ioctl( establishedConnectionFD, FIONREAD, &packetLength );
+	} while (packetLength > 0);
+
+	//cat buffer to messageIn
+	if( messageSize <= strlen(buffer) + strlen(messageIn) ) {
+		messageSize *= 2;
+		messageIn = realloc(messageIn, messageSize);
+	}
+	strcat(messageIn, buffer);
+
+	messageOut = encryptMessage(messageIn);
+
+	//send encrypted message
+	charsSent = send(establishedConnectionFD, messageOut, strlen(messageOut), 0);
+	if (charsSent < 0 ) { error("ERROR writing to socket"); }
+
+	//make sure output is sent
+	packetLength = -5;
+	do {
+		ioctl( establishedConnectionFD, TIOCOUTQ, &packetLength );
+	} while (packetLength > 0);
+
+	free(messageIn);
+	free(messageOut);
+
+	close(establishedConnectionFD);
+}
+
+
+
+/*
+		creates and returns a socket for listening
+*/
+int createListenSocket(struct sockaddr_in* serverAddress, int portNumber) {
+	int listenSocketFD;
+
+	//zero out serv addr struct
+	memset((char *)serverAddress, '\0', sizeof(*serverAddress));
+
+	// Set up the address struct for this process (the server)
+	serverAddress->sin_family = AF_INET;
+	serverAddress->sin_port = htons(portNumber);
+	serverAddress->sin_addr.s_addr = INADDR_ANY;
+
+	//set up listensocket
+	listenSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocketFD < 0 ) { error("ERROR opening socket"); }
+
+	//bind socket to port
+	if ( bind (listenSocketFD, (struct sockaddr*)serverAddress, sizeof(*serverAddress)) < 0 ) {
+		error("ERROR on binding");
+	}
+}
+
+
+
+//	cipher helper function
 int cipher_ctoi( char c) {
 	if( c == ' ') 
 		return 26;
@@ -127,6 +251,7 @@ int cipher_ctoi( char c) {
 		return c - 'A';
 }
 
+//	encryptMessage helper function
 char cipher(char msg_c, char key_c) {
 	int m, k, cipherInt;
 
@@ -142,6 +267,11 @@ char cipher(char msg_c, char key_c) {
 		return cipherInt + 'A';
 }
 
+
+/*
+	splits and incoming message with the @ symbol and encrypts the first half with the second
+	returns pointer to encrypted message
+*/
 char* encryptMessage(char* messageIn) {
 
 	int i;
